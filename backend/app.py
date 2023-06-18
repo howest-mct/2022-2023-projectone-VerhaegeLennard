4a476424f851051c8d2d2a7e6c10f3345379acd1
Lennard Verhaege <lennard.verhaege@student.howest.be>
2023-06-19 01:38:09 +0200

    feat: automatisch opstarten bugs fixed

--- backend/app.py
+++ backend/app.py
@@ -34,6 +34,7 @@ TVOC = None
 temperatuur = None
 luchtvochtigheid = None
 
+
 def toonOpLCD():
     lcd.send_instruction(0b00000001)  # wis het scherm
     # Haal de ip adressen op, split ze per ip
@@ -41,10 +42,13 @@ def toonOpLCD():
     ips = byte_ips.decode("utf-8")
     adresses = ips.split(" ")
     # stuur op de eerste lijn de 1e ip
-    lcd.write_message(adresses[0])
+    if len(adresses) >= 1:
+        lcd.write_message(adresses[0])
     # selecteer de 2de lijn en zend de 2de ip
-    lcd.send_instruction(0b11000000)
-    lcd.write_message(adresses[1])
+    if len(adresses) >= 2:
+        lcd.send_instruction(0b11000000)
+        lcd.write_message(adresses[1])
+
 
 def pushButtonLuik_callback(channel):
     global status_knop_luik
@@ -52,17 +56,20 @@ def pushButtonLuik_callback(channel):
     status_knop_luik = 1
     # print("LUIK")
 
+
 def pushButtonVoer_callback(channel):
     global status_knop_voer
     # if GPIO.input(channel) == 1:
     status_knop_voer = 1
     # print("VOER")
 
+
 def togglePowerButton_callback(channel):
     print("System shutting down...")
     lcd.init_LCD()
     lcd.write_message("System shutdown!")
     os.system("sudo shutdown now")
+
 
 def setup():
     toonOpLCD()
@@ -78,7 +85,7 @@ def setup():
                           pushButtonVoer_callback, bouncetime=200)
     GPIO.add_event_detect(togglePowerButton, GPIO.BOTH,
                           togglePowerButton_callback, bouncetime=200)
-    
+
 
 def run_hardware():
     global status_knop_luik, status_knop_voer, modus, settings
@@ -87,7 +94,8 @@ def run_hardware():
     modus = settings["Modus"]
     data = DataRepository.read_last_device_history(4)
     status_luik = data['ActieId']
-    tijdnu = (f"{datetime.fromtimestamp(time.time()).strftime('%H')}:{datetime.fromtimestamp(time.time()).strftime('%M')}")
+    tijdnu = (
+        f"{datetime.fromtimestamp(time.time()).strftime('%H')}:{datetime.fromtimestamp(time.time()).strftime('%M')}")
     executed_once_open = False
     executed_once_close = False
     executed_once_feed = False
@@ -102,7 +110,8 @@ def run_hardware():
             reset_var_last_run = now
 
         # print(settings)
-        tijdnu = (f"{datetime.fromtimestamp(time.time()).strftime('%H')}:{datetime.fromtimestamp(time.time()).strftime('%M')}")
+        tijdnu = (
+            f"{datetime.fromtimestamp(time.time()).strftime('%H')}:{datetime.fromtimestamp(time.time()).strftime('%M')}")
         if modus == 1:
             # print(settings)
             # print('manual mode')
@@ -118,7 +127,7 @@ def run_hardware():
             print('Geef voer')
             status_knop_voer = 1
             executed_once_feed = True
-        
+
         if modus == 0:
             # print('auto mode')
             if lichtintensiteit > 2000 and not executed_once_open and status_luik != 10:
@@ -174,6 +183,7 @@ def run_hardware():
                 device_id=5, actie_id=2, waarde=1, commentaar="Er werd 1 portie voer gegeven")
             status_knop_voer = 0
             toonOpLCD()
+
 
 # Custom endpoint
 ENDPOINT = '/api/v1'
@@ -238,20 +248,24 @@ def start_thread():
 def hallo():
     return "Server is running, er zijn momenteel geen API endpoints beschikbaar."
 
+
 @app.route(ENDPOINT + '/devices/', methods=['GET'])
 def get_devices():
     data = DataRepository.read_all_devices()
     return jsonify(data), 200
+
 
 @app.route(ENDPOINT + '/devices/<id>/', methods=['GET'])
 def get_device_by_id(id):
     data = DataRepository.read_history_by_deviceid(id)
     return jsonify(data), 200
 
+
 @app.route(ENDPOINT + '/timeline/', methods=['GET'])
 def get_timeline_history():
     data = DataRepository.read_history_timeline()
     return jsonify(data), 200
+
 
 @app.route(ENDPOINT + '/config/<user_id>/', methods=['GET'])
 def get_config(user_id):
@@ -264,17 +278,20 @@ def get_config(user_id):
 #     return jsonify(data), 200
 
 # SOCKET IO
+
+
 @socketio.on('connect')
 def initial_connection():
     global lichtintensiteit, eCO2, TVOC, temperatuur, luchtvochtigheid
     print('A new client connect')
-    socketio.emit('B2F_new_sensor_values', {'lichtintensiteit': lichtintensiteit, 'eCO2': eCO2, 'TVOC': TVOC, 'temperatuur': temperatuur, 'luchtvochtigheid': luchtvochtigheid})
+    socketio.emit('B2F_new_sensor_values', {'lichtintensiteit': lichtintensiteit, 'eCO2': eCO2,
+                  'TVOC': TVOC, 'temperatuur': temperatuur, 'luchtvochtigheid': luchtvochtigheid})
     data = DataRepository.read_last_device_history(4)
     laatste_status_deur = data["ActieId"]
     if laatste_status_deur == 10:
-        socketio.emit('B2F_current_door_icon',{'status': 'opened'})
+        socketio.emit('B2F_current_door_icon', {'status': 'opened'})
     if laatste_status_deur == 11:
-        socketio.emit('B2F_current_door_icon',{'status': 'closed'})
+        socketio.emit('B2F_current_door_icon', {'status': 'closed'})
     # # Send to the client!
     # vraag alle devices op uit de db
     # devices = DataRepository.read_all_devices()
@@ -293,27 +310,32 @@ def control_motor(data):
     elif motor_id == 'door':
         status_knop_luik = 1
 
+
 @socketio.on('F2B_new_config')
 def change_config(dict_settings):
     global settings
     if dict_settings['Modus'] == 'Manual':
         print('manueel')
         dict_settings['Modus'] = 1
-        DataRepository.update_config_full(dict_settings['user'], dict_settings['Modus'], dict_settings['OpenTijd'], dict_settings['SluitTijd'], dict_settings['VoederTijd'])
+        DataRepository.update_config_full(dict_settings['user'], dict_settings['Modus'],
+                                          dict_settings['OpenTijd'], dict_settings['SluitTijd'], dict_settings['VoederTijd'])
     if dict_settings['Modus'] == 'Auto':
         dict_settings['Modus'] = 0
         # print(dict_settings['VoederTijd'])
         print('auto')
-        DataRepository.update_config_small(dict_settings['user'], dict_settings['Modus'], dict_settings['VoederTijd'])
+        DataRepository.update_config_small(
+            dict_settings['user'], dict_settings['Modus'], dict_settings['VoederTijd'])
         dict_settings['OpenTijd'] = ''
         dict_settings['SluitTijd'] = ''
     settings = dict_settings
     print(settings)
     socketio.emit('B2F_config_update')
 
+
 @socketio.on('F2B_system_shutdown')
 def power_off():
     togglePowerButton_callback('app')
+
 
 if __name__ == '__main__':
     try:
